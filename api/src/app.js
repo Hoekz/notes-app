17f5de277c7a8be651bc29e420a548d5d77a8f2e
James Hoekzema <james.hoekzema@wwt.com>
2022-05-03 13:26:04 -0500

    super simple route - JH

--- api/src/app.js
+++ api/src/app.js
@@ -4,22 +4,10 @@ const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
-const passport = require('passport');
 const httpStatus = require('http-status');
-const config = require('./config/config');
-const morgan = require('./config/morgan');
-const { jwtStrategy } = require('./config/passport');
-const { authLimiter } = require('./middlewares/rateLimiter');
-const routes = require('./routes/v1');
-const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
+const routes = require('./routes');
 
 const app = express();
-
-if (config.env !== 'test') {
-  app.use(morgan.successHandler);
-  app.use(morgan.errorHandler);
-}
 
 // set security HTTP headers
 app.use(helmet());
@@ -49,32 +37,12 @@ app.use(compression());
 app.use(cors());
 app.options('*', cors());
 
-// jwt authentication
-app.use(passport.initialize());
-// JSON Web Token
-// abcdefg.dsaflsdf.dasfsdf
-// 1) { "alg": "sha256" }
-// 2) { "uid": "123456", "email": "email@domain.com", "exp": 12345678 }
-// 3) secret hash
-passport.use('jwt', jwtStrategy);
-
-// limit repeated failed requests to auth endpoints
-if (config.env === 'production') {
-  app.use('/v1/auth', authLimiter);
-}
-
 // v1 api routes
-app.use('/v1', routes);
+app.use('/', routes);
 
 // send back a 404 error for any unknown api request
-app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+app.use((_, res) => {
+  return res.status(httpStatus.NOT_FOUND).send('Not found');
 });
-
-// convert error to ApiError, if needed
-app.use(errorConverter);
-
-// handle error
-app.use(errorHandler);
 
 module.exports = app;
